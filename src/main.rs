e6c409026f3f560b14b7f459abc3cba1b5098641
0xOmarA <OmarAbdulla7@hotmail.com>
2022-06-12 13:36:19 +0300

    Disable overflow checks from the game

--- src/main.rs
+++ src/main.rs
@@ -6,11 +6,27 @@ mod coordinate;
 mod piece;
 
 fn main() {
-    let mut board: board::Board = board::Board::new_with_fen(Fen { state: "8/8/8/4p1K1/2k1P3/8/8/8 b - - 0 1".to_string() });
+    let mut board: board::Board = board::Board::new();
     
-    // let from: Coordinate = Coordinate::try_from("E2").unwrap();
-    // let to: Coordinate = Coordinate::try_from("E4").unwrap();
-    // println!("Response is: {:?}", board.move_piece(&from, &to));
+    let from: Coordinate = Coordinate::try_from("E2").unwrap();
+    let to: Coordinate = Coordinate::try_from("E4").unwrap();
+    println!("Response is: {:?}", board.move_piece(&from, &to));
+    
+    let from: Coordinate = Coordinate::try_from("E7").unwrap();
+    let to: Coordinate = Coordinate::try_from("E5").unwrap();
+    println!("Response is: {:?}", board.move_piece(&from, &to));
+    
+    let from: Coordinate = Coordinate::try_from("D2").unwrap();
+    let to: Coordinate = Coordinate::try_from("D4").unwrap();
+    println!("Response is: {:?}", board.move_piece(&from, &to));
+    
+    let from: Coordinate = Coordinate::try_from("D7").unwrap();
+    let to: Coordinate = Coordinate::try_from("D5").unwrap();
+    println!("Response is: {:?}", board.move_piece(&from, &to));
+    
+    let from: Coordinate = Coordinate::try_from("C1").unwrap();
+    let to: Coordinate = Coordinate::try_from("E3").unwrap();
+    println!("Response is: {:?}", board.move_piece(&from, &to));
     
     println!("{}", board);
     let fen: Fen = board.fen();
